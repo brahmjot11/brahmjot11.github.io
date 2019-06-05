107924fce6b054334f3eb693629bfe61068dba36
Kanika <imkanikagarg@gmail.com>
2019-06-06 00:33:16 +0530

    5

--- whatwedo/html/js/scripts.js
+++ whatwedo/html/js/scripts.js
@@ -9,6 +9,56 @@
     function my3Function() { 
                 window.open('../../design/HTML/SITE/index.html', "_top" ); 
             }
+
+
+$('#carouselExample').on('slide.bs.carousel', function (e) {
+
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
+
+
+//   $(document).ready(function() {
+//   $("#myCarousel").on("slide.bs.carousel", function(e) {
+//     var $e = $(e.relatedTarget);
+//     var idx = $e.index();
+//     var itemsPerSlide = 3;
+//     var totalItems = $(".carousel-item").length;
+
+//     if (idx >= totalItems - (itemsPerSlide - 1)) {
+//       var it = itemsPerSlide - (totalItems - idx);
+//       for (var i = 0; i < it; i++) {
+       
+//         if (e.direction == "left") {
+//           $(".carousel-item")
+//             .eq(i)
+//             .appendTo(".carousel-inner");
+//         } else {
+//           $(".carousel-item")
+//             .eq(0)
+//             .appendTo($(this).find(".carousel-inner"));
+//         }
+//       }
+//     }
+//   });
+// });
+
+      
 (function($){
 
   "use strict";
